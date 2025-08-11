6e85329ddc8202c8fc087dee259aa32239bca717
rolfosian <135147406+rolfosian@users.noreply.github.com>
2025-08-11 20:09:22 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from winreg import OpenKey, EnumValue, HKEY_CURRENT_USER
-key_path = r"SOFTWARE\HWiNFO64\VSB"
-def get_hardware_stats():
+
+KEY_PATH = r"SOFTWARE\HWiNFO64\VSB"
+SENSOR = "Sensor"
+LABEL = "Label"
+VALUE = "Value"
+RAW = "Raw"
+
+def get_hardware_stats() -> dict:
     hwinfo = {}
     
-    with OpenKey(HKEY_CURRENT_USER, key_path) as key:
+    with OpenKey(HKEY_CURRENT_USER, KEY_PATH) as key: # this will throw if you dont have HWINFO64 running with sensors and gadget entries enabled so maybe wrap in try except
         index = 0
-        curr_m = None
+        curr_master = None
         curr_label = None
         
         while True:
@@ -13,17 +19,18 @@ def get_hardware_stats():
                 value_name, value_data, _ = EnumValue(key, index)
                 index += 1
                 
-                if value_name.startswith('Sensor'):
-                    curr_m = value_data
-                    if curr_m not in hwinfo:
-                        hwinfo[curr_m] = {}
+                if value_name.startswith(SENSOR):
+                    curr_master = value_data
+                    if curr_master not in hwinfo:
+                        hwinfo[curr_master] = {}
                     continue
                 
-                if value_name.startswith('Label'):
+                if value_name.startswith(LABEL):
                     curr_label = value_data
                 
-                if value_name.startswith('Value') and 'Raw' not in value_name:
-                    hwinfo[curr_m][curr_label] = value_data
+                if value_name.startswith(VALUE) and RAW not in value_name:
+                    hwinfo[curr_master][curr_label] = value_data
+                    
             except OSError:
                 break
     
